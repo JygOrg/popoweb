ee353ba1ff9a51e19f51b9053c74c404198b7654
Justin Cheung <jcheung@crunchyroll.com>
2015-02-27 15:54:10 -0800

    [NEW] Implement basic node/express server

---
+++ server.js
@@ -0,0 +1,19 @@
+// initialize express
+var express = require('express');
+var app = express();
+
+// configure express
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+// define routes
+app.get('/world', function(req, res) {
+  res.status(200).send("Hello, World!");
+});
+
+// start listener
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
+
+console.log("amazing");
